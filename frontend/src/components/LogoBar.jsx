867aff4c9307fc6f64ba818e7cf1794993424eba
Lucas Chaves <231026456@aluno.unb.br>
2025-07-03 16:26:32 -0300

    Estilização componente LogoBar

--- frontend/src/components/LogoBar.jsx
+++ frontend/src/components/LogoBar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../assets/logo brasil-participativo.png";
+import Logo from "../assets/Logo BP.svg";
 export function LogoBar() {
   return (
     <div className=" bg-white border-b border-gray-200">
       <div className="  max-w-7xl mx-auto py-4 flex justify-center">
         <img
-          src={logo}
-          className="h-12"
+          src={Logo}
+          className="h-24"
         />
       </div>
     </div>
