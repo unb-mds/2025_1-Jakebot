2de68ed55a736ef148880c1e3440367fcc69add6
EduardoRibeiroXavier <EduardoRibeiroXavier@users.noreply.github.com>
2025-07-10 23:49:14 +0000

    deploy: f3044c3583e5043808e7d9ad59e955719ecd3dd3

--- backend/bots/sentiment_bot/app/main.py
+++ backend/bots/sentiment_bot/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://jakebot.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
